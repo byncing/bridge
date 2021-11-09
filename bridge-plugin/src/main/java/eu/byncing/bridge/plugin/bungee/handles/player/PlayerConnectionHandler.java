7f26b70a6f04219a238b8ad9eb74e15ca9310f89
byncing <byncing@gmail.com>
2021-11-09 23:33:32 +0100

    UniqueId stuff removed

--- bridge-plugin/src/main/java/eu/byncing/bridge/plugin/bungee/handles/player/PlayerConnectionHandler.java
+++ bridge-plugin/src/main/java/eu/byncing/bridge/plugin/bungee/handles/player/PlayerConnectionHandler.java
@@ -6,8 +6,8 @@ import eu.byncing.bridge.driver.protocol.IPacketHandler;
 import eu.byncing.bridge.driver.protocol.packets.player.PacketPlayerConnect;
 import eu.byncing.bridge.driver.protocol.packets.player.PacketPlayerDisconnect;
 import eu.byncing.bridge.plugin.BridgeService;
-import eu.byncing.bridge.plugin.bungee.impl.BridgePlayer;
 import eu.byncing.bridge.plugin.bungee.BridgeServer;
+import eu.byncing.bridge.plugin.bungee.impl.BridgePlayer;
 import eu.byncing.net.api.channel.IChannel;
 import eu.byncing.net.api.protocol.Packet;
 
@@ -24,22 +24,22 @@ public class PlayerConnectionHandler implements IPacketHandler<Packet> {
         if (packet instanceof PacketPlayerConnect) {
             PacketPlayerConnect connect = (PacketPlayerConnect) packet;
             BridgePlayer player = (BridgePlayer) server.getPlayers().getPlayer(connect.getUniqueId());
-            BridgeService service = (BridgeService) server.getServices().getService(connect.getServiceUniqueId());
+            BridgeService service = (BridgeService) server.getServices().getService(connect.getService());
             service.getPlayers().add(player);
             player.setService(service);
-            server.sendPacket(new PacketPlayerConnect(player.getUniqueId(), player.getName(), service.getUniqueId()));
+            server.sendPacket(new PacketPlayerConnect(player.getUniqueId(), player.getName(), service.getName()));
             server.sendMessage("Player " + player.getName() + " has connected with the " + service.getName() + " service.");
             server.getEvents().call(new PlayerConnectEvent(player, service));
         }
         if (packet instanceof PacketPlayerDisconnect) {
             PacketPlayerDisconnect disconnect = (PacketPlayerDisconnect) packet;
             BridgePlayer player = (BridgePlayer) server.getPlayers().getPlayer(disconnect.getUniqueId());
-            BridgeService service = (BridgeService) server.getServices().getService(disconnect.getServiceUniqueId());
+            BridgeService service = (BridgeService) server.getServices().getService(disconnect.getService());
             service.getPlayers().remove(player);
             player.setService(service);
             server.sendMessage("Player " + player.getName() + " has disconnected with the " + service.getName() + " service.");
             server.getEvents().call(new PlayerDisconnectEvent(player, service));
-            server.sendPacket(new PacketPlayerDisconnect(player.getUniqueId(), player.getName(), service.getUniqueId()));
+            server.sendPacket(new PacketPlayerDisconnect(player.getUniqueId(), player.getName(), service.getName()));
         }
     }
 
