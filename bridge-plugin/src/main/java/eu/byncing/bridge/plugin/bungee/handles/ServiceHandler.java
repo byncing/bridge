f88333d4e61ad02e78686061293ea48e982c016c
byncing <byncing@gmail.com>
2021-11-09 23:35:11 +0100

    Better authentication

--- bridge-plugin/src/main/java/eu/byncing/bridge/plugin/bungee/handles/ServiceHandler.java
+++ bridge-plugin/src/main/java/eu/byncing/bridge/plugin/bungee/handles/ServiceHandler.java
@@ -1,21 +1,22 @@
 package eu.byncing.bridge.plugin.bungee.handles;
 
+import eu.byncing.bridge.driver.BridgeUtil;
 import eu.byncing.bridge.driver.event.service.ServiceLoginEvent;
 import eu.byncing.bridge.driver.event.service.ServiceUpdateEvent;
 import eu.byncing.bridge.driver.protocol.IPacketHandler;
 import eu.byncing.bridge.driver.protocol.packets.player.PacketPlayerUpdate;
 import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceAuth;
-import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceAuthState;
+import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceAuthFailed;
 import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceLogin;
 import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceUpdate;
 import eu.byncing.bridge.driver.service.IBridgeService;
-import eu.byncing.bridge.plugin.bungee.BridgeServer;
 import eu.byncing.bridge.plugin.BridgeService;
+import eu.byncing.bridge.plugin.bungee.BridgeServer;
 import eu.byncing.net.api.channel.IChannel;
 import eu.byncing.net.api.protocol.Packet;
+import net.md_5.bungee.api.ProxyServer;
 
 import java.io.IOException;
-import java.util.UUID;
 
 public class ServiceHandler implements IPacketHandler<Packet> {
 
@@ -31,27 +32,43 @@ public class ServiceHandler implements IPacketHandler<Packet> {
             try {
                 PacketServiceAuth auth = (PacketServiceAuth) packet;
                 if (auth.getKey().equals(server.getConfig().getKey())) {
-                    IBridgeService service = new BridgeService(channel, UUID.randomUUID(), auth.getName(), auth.getMotd(), auth.getMaxCount());
+
+                    if (server.getServices().getService(auth.getName()) != null) {
+                        channel.sendPacket(new PacketServiceAuthFailed(BridgeUtil.builder("§cThe specified service already exists!").replace("&", "§", "Â").buildIndex(0)));
+                        channel.close();
+                        return;
+                    }
+
+                    if(ProxyServer.getInstance().getServerInfo(auth.getName()) == null) {
+                        channel.sendPacket(new PacketServiceAuthFailed(BridgeUtil.builder("§cThe service is not entered in the bungee config!").replace("&", "§", "Â").buildIndex(0)));
+                        channel.close();
+                        return;
+                    }
+
+                    IBridgeService service = new BridgeService(channel, auth.getName(), auth.getMotd(), auth.getMaxCount());
                     server.sendMessage("Channel" + channel.getRemoteAddress() + " Service " + service.getName() + " has login.");
                     server.getServices().getServices().add(service);
                     server.getEvents().call(new ServiceLoginEvent(service));
-                    channel.sendPacket(new PacketServiceAuthState(PacketServiceAuthState.State.ACCEPT, service.getUniqueId(), service.getName(), service.getMotd(), service.getOnlineCount(), service.getMaxCount()));
-                    server.getServices().getServices().forEach(service1 -> server.sendPacket(new PacketServiceUpdate(service1.getUniqueId(), service1.getName(), service1.getMotd(), service1.getOnlineCount(), service1.getMaxCount())));
+                    server.getServices().getServices().forEach(service1 -> server.sendPacket(new PacketServiceUpdate(service1.getName(), service1.getMotd(), service1.getOnlineCount(), service1.getMaxCount())));
                     server.getPlayers().getPlayers().forEach(player -> {
                         if (player.getService() != null) {
-                            server.sendPacket(new PacketPlayerUpdate(player.getUniqueId(), player.getName(), player.getService().getUniqueId()));
+                            server.sendPacket(new PacketPlayerUpdate(player.getUniqueId(), player.getName(), player.getService().getName()));
                         }
                     });
-                    server.sendPacket(new PacketServiceLogin(service.getUniqueId(), service.getName(), service.getMotd(), service.getOnlineCount(), service.getMaxCount()));
+                    server.sendPacket(new PacketServiceLogin(service.getName(), service.getMotd(), service.getOnlineCount(), service.getMaxCount()));
 
-                } else channel.close();
+                } else {
+                    System.out.println(channel.isConnected());
+                    channel.sendPacket(new PacketServiceAuthFailed(BridgeUtil.builder("§cThe specified key does not match :/").replace("&", "§", "Â").buildIndex(0)));
+                    channel.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         if (packet instanceof PacketServiceUpdate) {
             PacketServiceUpdate update = (PacketServiceUpdate) packet;
-            BridgeService service = (BridgeService) server.getServices().getService(update.getUniqueId());
+            BridgeService service = (BridgeService) server.getServices().getService(update.getName());
             service.update(update);
             server.getEvents().call(new ServiceUpdateEvent(service));
             server.sendPacket(update);
