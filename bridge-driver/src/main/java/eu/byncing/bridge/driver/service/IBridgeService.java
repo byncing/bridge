b4bd85a342e4f0334ed6412595fc53c39665cceb
byncing <byncing@gmail.com>
2021-11-09 23:30:40 +0100

    Removed uniqueId

--- bridge-driver/src/main/java/eu/byncing/bridge/driver/service/IBridgeService.java
+++ bridge-driver/src/main/java/eu/byncing/bridge/driver/service/IBridgeService.java
@@ -8,7 +8,6 @@ import eu.byncing.net.api.protocol.Packet;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 public interface IBridgeService extends Closeable, IPacketSender {
 
@@ -19,8 +18,6 @@ public interface IBridgeService extends Closeable, IPacketSender {
     void sendPacket(Packet packet);
 
     IChannel getChannel();
-
-    UUID getUniqueId();
 
     String getName();
 
