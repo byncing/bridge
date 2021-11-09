3413ad918562e1aa497653e80d9153675533e957
byncing <byncing@gmail.com>
2021-11-09 23:38:53 +0100

    Better authentication

--- bridge-plugin/src/main/java/eu/byncing/bridge/plugin/spigot/handles/service/ServiceHandler.java
+++ bridge-plugin/src/main/java/eu/byncing/bridge/plugin/spigot/handles/service/ServiceHandler.java
@@ -1,7 +1,9 @@
 package eu.byncing.bridge.plugin.spigot.handles.service;
 
+import eu.byncing.bridge.driver.BridgeUtil;
 import eu.byncing.bridge.driver.event.service.ServiceUpdateEvent;
 import eu.byncing.bridge.driver.protocol.IPacketHandler;
+import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceAuthFailed;
 import eu.byncing.bridge.driver.protocol.packets.service.PacketServiceUpdate;
 import eu.byncing.bridge.plugin.BridgeService;
 import eu.byncing.bridge.plugin.spigot.BridgeClient;
@@ -18,16 +20,20 @@ public class ServiceHandler implements IPacketHandler<Packet> {
 
     @Override
     public void handle(IChannel channel, Packet packet) {
+        if (packet instanceof PacketServiceAuthFailed) {
+            PacketServiceAuthFailed failed = (PacketServiceAuthFailed) packet;
+            client.sendMessage("Failed: " + BridgeUtil.builder(failed.getReason()).replace("§", "&").buildIndex(0));
+        }
         if (packet instanceof PacketServiceUpdate) {
             PacketServiceUpdate update = (PacketServiceUpdate) packet;
             BridgeService service;
-            if (client.getServices().getService(update.getUniqueId()) == null) {
-                service = new BridgeService(channel, update.getUniqueId(), update.getName(), update.getMotd(), update.getOnlineCount(), update.getMaxCount());
+            if (client.getServices().getService(update.getName()) == null) {
+                service = new BridgeService(channel, update.getName(), update.getMotd(), update.getOnlineCount(), update.getMaxCount());
                 client.getServices().getServices().add(service);
                 client.getEvents().call(new ServiceUpdateEvent(service));
                 return;
             }
-            service = (BridgeService) client.getServices().getService(update.getUniqueId());
+            service = (BridgeService) client.getServices().getService(update.getName());
             service.update(update);
             client.getEvents().call(new ServiceUpdateEvent(service));
         }
@@ -35,6 +41,6 @@ public class ServiceHandler implements IPacketHandler<Packet> {
 
     @Override
     public Class<? extends Packet>[] getClasses() {
-        return new Class[]{PacketServiceUpdate.class};
+        return new Class[]{PacketServiceUpdate.class, PacketServiceAuthFailed.class};
     }
 }
