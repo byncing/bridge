7aeb2099c95afff1662ae956d1415c0b45f0db58
byncing <byncing@gmail.com>
2021-11-14 19:37:50 +0100

    Update example

--- bridge-examples/src/main/java/eu/byncing/bridge/examples/spigot/SpigotListener.java
+++ bridge-examples/src/main/java/eu/byncing/bridge/examples/spigot/SpigotListener.java
@@ -1,6 +1,19 @@
 package eu.byncing.bridge.examples.spigot;
 
+import eu.byncing.bridge.driver.event.BridgeHandler;
 import eu.byncing.bridge.driver.event.IBridgeListener;
+import eu.byncing.bridge.driver.event.player.PlayerConnectEvent;
+import eu.byncing.bridge.driver.player.IBridgePlayer;
+import eu.byncing.bridge.driver.service.IBridgeService;
 
 public class SpigotListener implements IBridgeListener {
+
+    @BridgeHandler(internal = true)
+    public void handle(PlayerConnectEvent event) {
+        IBridgePlayer player = event.getPlayer();
+        IBridgeService service = event.getService();
+
+        player.sendMessage(player.getService().getName());
+        player.sendMessage(service.getName());
+    }
 }
