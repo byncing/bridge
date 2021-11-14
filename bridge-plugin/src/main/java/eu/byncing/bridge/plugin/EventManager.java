f6e73fd2ff9990bfcabd093899c9344dc482191c
byncing <byncing@gmail.com>
2021-11-14 19:41:35 +0100

    Support of internal boolean

--- bridge-plugin/src/main/java/eu/byncing/bridge/plugin/EventManager.java
+++ bridge-plugin/src/main/java/eu/byncing/bridge/plugin/EventManager.java
@@ -1,4 +1,12 @@
-package eu.byncing.bridge.driver.event;
+package eu.byncing.bridge.plugin;
+
+import eu.byncing.bridge.driver.BridgeDriver;
+import eu.byncing.bridge.driver.IBridgeDriver;
+import eu.byncing.bridge.driver.event.BridgeHandler;
+import eu.byncing.bridge.driver.event.IBridgeListener;
+import eu.byncing.bridge.driver.event.IEvent;
+import eu.byncing.bridge.driver.event.IEventManager;
+import eu.byncing.bridge.driver.service.IBridgeService;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -7,21 +15,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class EventManager {
+public class EventManager implements IEventManager {
 
     private final List<IBridgeListener> listeners = new ArrayList<>();
 
+    private IBridgeService service;
+
+    public void setService(IBridgeService service) {
+        this.service = service;
+    }
+
+    @Override
     public void register(IBridgeListener... listeners) {
         this.listeners.addAll(Arrays.asList(listeners));
     }
 
+    @Override
+    public void unregister(IBridgeListener... listeners) {
+        this.listeners.removeAll(Arrays.asList(listeners));
+    }
+
+    @Override
     public void call(IBridgeListener listener, IEvent event) {
         try {
+            IBridgeDriver instance = BridgeDriver.getInstance();
             for (Method method : listener.getClass().getMethods()) {
                 BridgeHandler handler = method.getAnnotation(BridgeHandler.class);
                 if (handler == null) return;
                 Parameter[] parameters = method.getParameters();
                 if (Arrays.stream(parameters).anyMatch(parameter -> parameter.getType().equals(event.getClass()))) {
+                    if (handler.internal()) {
+                        if (service == null) {
+                            method.invoke(listener, event);
+                            return;
+                        }
+                        if (service.equals(instance.getInternalService())) method.invoke(listener, event);
+                        return;
+                    }
                     method.invoke(listener, event);
                 }
             }
@@ -30,10 +60,12 @@ public class EventManager {
         }
     }
 
+    @Override
     public void call(IEvent event) {
         listeners.forEach(listener -> call(listener, event));
     }
 
+    @Override
     public List<IBridgeListener> getListeners() {
         return listeners;
     }
